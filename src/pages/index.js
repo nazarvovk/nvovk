31a1c29da3f2dfb41ab7913c1a92d57edc0b1af3
Nazar Vovk <nazarvovk.work@gmail.com>
2024-03-06 15:06:39 +0200

    feat

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import './global.scss';
 import React from 'react';
-import Footer from 'components/Footer/Footer';
 import SEO from 'components/seo';
 import styles from './index.module.scss';
 
@@ -12,22 +11,20 @@ const HomePage = () => {
         <div className={styles.Container}>
           <div className={styles.Spacer}>
             <p>
-              Not available for work due to a genocide being committed by
-              russians in Ukraine.
+              At some point there will be a new website here.
             </p>
-            <p>russia is a terrorist state.</p>
             <br />
             <p>
               <a
                 target="_blank"
                 rel="noreferrer"
                 href="https://savelife.in.ua/en/donate-en/"
+                style={{ textDecoration: 'underline' }}
               >
                 Donate to Come Back Alive Foundation.
               </a>
             </p>
           </div>
-          <Footer />
         </div>
       </main>
     </>
