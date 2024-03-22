ec86ca5160894e6c61451e675e055af3a9e65f3e
Nazar Vovk <nazarvovk.work@gmail.com>
2024-03-22 16:45:39 +0200

    feat: next init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,45 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
 module.exports = {
-  env: {
-    node: true,
-    browser: true,
-    es6: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['react', 'react-hooks'],
+  extends: [
+    'prettier',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:jest/recommended',
+  ],
+  plugins: ['prettier'],
   rules: {
-    'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'lf',
+        printWidth: 100,
+        semi: false,
+        singleQuote: true,
+        jsxSingleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-unused-vars': 'off',
+    'no-debugger': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+      },
+    ],
+    'jest/valid-title': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    'no-unreachable': 'error',
   },
-};
+  settings: {
+    tailwindcss: {
+      callees: ['cn'],
+    },
+  },
+}
