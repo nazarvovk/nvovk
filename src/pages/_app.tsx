d898adf88e325101e4b768affe5859b5e0dcac1a
Nazar Vovk <nazarvovk.work@gmail.com>
2025-11-18 14:14:28 +0000

    feat: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { Layout } from '@/components/layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <SpeedInsights />
+      <Analytics />
       <Component {...pageProps} />
     </Layout>
   )
