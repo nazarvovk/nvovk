e25afc8ab6c8a4ef8643af3320ef497f67c2fe8b
Nazar Vovk <nazarvovk.work@gmail.com>
2025-12-03 20:10:43 +0000

    fix: upgrade all packages fixes cve-2025-55182

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,8 +1,11 @@
-import { ContextWithValidatedRequestBody, formRequestMiddleware } from '@/form-request-middleware'
-import { MainScope } from '@/scope'
-import { NextApiHandler } from 'next'
-import { shrext } from 'shrext'
-import { z } from 'zod'
+import {
+  ContextWithValidatedRequestBody,
+  formRequestMiddleware,
+} from '@/form-request-middleware';
+import { MainScope } from '@/scope';
+import { NextApiHandler } from 'next';
+import { shrext } from 'shrext';
+import { z } from 'zod';
 
 export const ContactInputSchema = z.object({
   name: z.string().min(2).max(100),
@@ -10,20 +13,25 @@ export const ContactInputSchema = z.object({
   message: z
     .string()
     .min(10)
-    .max(10000, 'Max 10,000 characters. Are you trying to send me the entire Bee Movie script?'),
-})
-export type ContactInput = z.infer<typeof ContactInputSchema>
+    .max(
+      10000,
+      'Max 10,000 characters. Are you trying to send me the entire Bee Movie script?'
+    ),
+});
+export type ContactInput = z.infer<typeof ContactInputSchema>;
 
-type ContactContext = ContextWithValidatedRequestBody<typeof ContactInputSchema>
+type ContactContext = ContextWithValidatedRequestBody<
+  typeof ContactInputSchema
+>;
 
 const handler = shrext<NextApiHandler, ContactContext>()
   .use(formRequestMiddleware(ContactInputSchema))
-  .handler(async ({ args: [, res], validatedBody }) => {
-    const scope = new MainScope()
+  .setHandler(async ({ args: [, res], validatedBody }) => {
+    const scope = new MainScope();
 
-    await scope.container.contactService.send(validatedBody)
+    await scope.container.contactService.send(validatedBody);
 
-    res.status(200).end()
-  })
+    res.status(200).end();
+  });
 
-export default handler
+export default handler;
